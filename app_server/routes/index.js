372258ecea83ba07cc144a45b4d0d2be0ff30616
Akshaya1313 <akshayapotlapally@gmail.com>
2025-08-21 20:53:34 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'loc8r' });
+});
+router.get('/location', function(req, res, next) 
+{
+  res.render('index',{title:'location info'});
+
+});
+router.get('/location/review', function(req, res, next) 
+{
+   res.render('index', { title: 'review' });
+
+});
+router.get('/about', function(req, res, next) 
+{
+   res.render('index', { title: 'about' });
 });
 
-module.exports = router;
+module.experts=router;
+
+
+
+
+
